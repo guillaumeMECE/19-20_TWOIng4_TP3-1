9ffc34bfbfc72091c4d2f82ebc082ffd0a7f2a58
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-10 15:54:58 +0100

    intial commit

---
+++ src/components/square/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default (props) => {
+    return (
+        <button className="square"
+            onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
