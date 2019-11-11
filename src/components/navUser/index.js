3b43a70b3e7369287ec371a5b2a7935b01c23146
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-11 13:19:42 +0100

    Handle click navUser

--- src/components/navUser/index.js
+++ src/components/navUser/index.js
@@ -7,22 +7,31 @@ import Profil from '../profil'
 export default class NavUser extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            focus: 0
+        };
+    }
+
+    changeFocus(id) {
+        this.setState({ focus: id })
     }
 
     renderUsers(user) {
-        return <Thumbnail key={user.id} user={user} />;
+        const result = <Thumbnail user={user} onClick={() => this.changeFocus(user.id)} />;
+        return result;
     }
 
     render() {
-        let usersList = data.users.map(this.renderUsers)
+        let usersList = data.users.map((user) => this.renderUsers(user))
+        console.log('usersList: ', usersList);
 
         return (
             <div className="navUser">
                 <div className="navbarUser">
                     {usersList}
                 </div>
-                <Profil user={data.users[0]} />
+                <Profil user={data.users[this.state.focus]} />
+                <Thumbnail key={data.users[1].id} user={data.users[1]} onClick={() => this.changeFocus(data.users[1].id)} />;
             </div>
         );
     }
