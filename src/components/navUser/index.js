0d28768f610987dc23a6dbe538c0d1e425fd4d92
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-11 11:48:19 +0100

    Nav user up

--- src/components/navUser/index.js
+++ src/components/navUser/index.js
@@ -2,54 +2,27 @@ import React, { Component } from 'react'
 import './style.css';
 import data from '../../assets/data.json'
 import Thumbnail from '../thumbnail'
+import Profil from '../profil'
 
 export default class NavUser extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            stepNumber: 0,
-            xIsNext: true,
-        };
+        this.state = {};
     }
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        // if (calculateWinner(squares) || squares[i]) {
-        //     return;
-        // }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares,
-            }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    ReadData() {
-        console.log('data', data);
-        console.log('data', data.users[0]);
+    renderUsers(user) {
+        return <Thumbnail key={user.id} user={user} />;
     }
 
     render() {
-        this.ReadData();
+        let usersList = data.users.map(this.renderUsers)
+
         return (
             <div className="navUser">
-                <Thumbnail
-                    user={data.users[0]}
-                />
-                <Thumbnail
-                    user={data.users[1]}
-                />
-                <Thumbnail
-                    user={data.users[2]}
-                />
+                <div className="navbarUser">
+                    {usersList}
+                </div>
+                <Profil user={data.users[0]} />
             </div>
         );
     }
